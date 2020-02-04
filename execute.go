eed588ed0f4cb237f71b8640ba3ce4833b10fa35
Ondrej Holecek <ondrej@holecek.eu>
2020-02-04 22:30:35 +0100

    added license header to files where they were missing

--- execute.go
+++ execute.go
@@ -1,3 +1,6 @@
+// Copyright 2020 Ondrej Holecek <ondrej@holecek.eu>. All rights reserved. Use of this source code
+// is governed by the CC BY-ND 4.0 license that can be found in the LICENSE.txt file.
+
 package main
 
 import (
--- plugins/stats/counter.go
+++ plugins/stats/counter.go
@@ -1,3 +1,6 @@
+// Copyright 2020 Ondrej Holecek <ondrej@holecek.eu>. All rights reserved. Use of this source code
+// is governed by the CC BY-ND 4.0 license that can be found in the LICENSE.txt file.
+
 // Counter struct is quite generic counter whose `key` can be any object (interface{} that can be map index),
 // and `value` is uint64 counter that can be increased by any value.
 //
